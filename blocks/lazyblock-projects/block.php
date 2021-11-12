df51574ea3240ae52e61f2d289244cd9c05a15f6
NorrDesign <83650311+NorrDesign@users.noreply.github.com>
2021-11-12 13:15:50 +0200

    Add files via upload

---
+++ blocks/lazyblock-projects/block.php
@@ -0,0 +1,34 @@
+<div class="container">
+    <div class="row">
+    <?php
+    $args = array(
+        'post_type' => 'project',
+        'posts_per_page' => -1
+    );
+
+
+    $query = new WP_Query($args);
+       
+    if ($query->have_posts() ) : 
+        
+        while ( $query->have_posts() ) : $query->the_post();?>
+        
+        <div class="col-12 col-sm-6 col-md-4 project">
+                <a href="<?php the_permalink();?>">
+                <div class="bcgr-project-image" style="background:url('<?php the_post_thumbnail_url('large'); ?>')"></div>
+              
+            
+            
+               <h3><?php the_title();?></h3> 
+                   
+            <?php the_excerpt();?>
+              </a> 
+            
+            </div>
+       <?php endwhile;
+
+        wp_reset_postdata();
+    endif;
+    ?>
+        </div>
+</div>
