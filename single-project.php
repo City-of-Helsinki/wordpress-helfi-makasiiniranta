5d874daa61ad0f91fbad7746d979ec58d467d09c
NorrDesign <83650311+NorrDesign@users.noreply.github.com>
2021-11-17 11:34:53 +0200

    Add files via upload

---
+++ single-project.php
@@ -0,0 +1,44 @@
+<?php
+get_header();
+
+/**
+  * Hook: helsinki_content_before
+  *
+  */
+do_action("helsinki_content_before");
+
+while ( have_posts() ) {
+
+	the_post();
+
+	/**
+      * Hook: helsinki_content
+      *
+      */
+    do_action("helsinki_content");
+
+}
+
+?>
+
+<div class="content__container hds-container hds-container--narrow">
+
+    <div class="load-pdf-cont">
+        
+        <?php if(get_field("pdf")){ ?>
+               <a class="a-button" href="<?php the_field("pdf");?>" target="_blank"> Load pdf</a>
+        <?php } ?>
+        
+        
+    </div>
+    
+    </div>
+<?php
+
+/**
+  * Hook: helsinki_content_after
+  *
+  */
+do_action("helsinki_content_after");
+
+get_footer();
