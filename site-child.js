514baaa40e89bdea1035594d93e39fd7161a14ab
NorrDev <83650311+NorrDesign@users.noreply.github.com>
2022-06-07 11:51:45 +0200

    Update site-child.js

--- site-child.js
+++ site-child.js
@@ -4,7 +4,7 @@
 document.addEventListener("DOMContentLoaded", function(){
     jQuery(document).ready(function($){
 
-     $("main>#recent-posts").hide();
+    // $("main>#recent-posts").hide();
         
         $(".hds-container--narrow .wp-block-lazyblock-wave-mask .hds-container").addClass("hds-container--narrow");
     $("html[lang='en-US'] #recent-post-container .container__heading").html("News");
